2d33503ce44eae3f31effd2eadc3adb4cd2bd963
Jiangwei-shi <56906163+Jiangwei-shi@users.noreply.github.com>
2023-04-20 20:10:10 -0400

    initial project

---
+++ src/tuiter/post-summary-list/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PostSummaryItem
+  from "./post-summary-item";
+import {useSelector} from "react-redux";
+const PostSummaryList = () => {
+  // const postsArray = useSelector(state => state.tuits)
+  const {tuits, loading} = useSelector(state => state.tuitsData)
+  return(
+    <ul className="list-group">
+      {
+        loading &&
+        <li className="list-group-item">
+          Loading...
+        </li>
+      }
+      {
+        tuits.map(post =>
+          <PostSummaryItem
+            key={post._id} post={post}/>)
+      }
+    </ul>
+  );
+};
+export default PostSummaryList;
+
---
+++ src/tuiter/tuits/TuitsList.js
@@ -0,0 +1,27 @@
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import TuitItem from "./TuitItem";
+import {findTuitsThunk}
+  from "../../services/tuits-thunks";
+
+const TuitsList = () => {
+  const {tuits, loading} = useSelector(state => state.tuitsData)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(findTuitsThunk())
+  }, [])
+  return(
+    <ul className="list-group">
+      {
+        loading &&
+        <li className="list-group-item">
+          Loading...
+        </li>
+      }
+      {
+        tuits.map((post) => <TuitItem key={post._id} post={post}/>)
+      }
+    </ul>
+  );
+};
+export default TuitsList;
