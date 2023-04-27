5008eeba4f73dd7ab67787e1bcdd076c53f15c40
Wei Zhang <zhang.wei8@northeastern.edu>
2023-04-27 07:40:44 -0400

    Change the reviews align to vertical.

--- src/tuiter/details/review-list.js
+++ src/tuiter/details/review-list.js
@@ -4,7 +4,7 @@ import ReviewItem from './review-item';
 const ReviewList = ({ reviews }) => {
   return (
     <div className='container'>
-      <div className='row'>
+      <div className='col'>
         {reviews.map(review => (
           <div className='col-md-6 mb-4' key={review.id}>
             <ReviewItem review={review} />
